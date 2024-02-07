fa27cf4246bd8f42908d20c4b048070c10eb59f6
HongMinha <131649405+minhahamin@users.noreply.github.com>
2024-02-07 22:22:28 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // 두 정수 A와 B를 입력받음
+        
+        // 두 정수 A와 B를 입력 받음
         int A = scanner.nextInt();
         int B = scanner.nextInt();
-
-        // A와 B를 더한 결과를 출력
-        System.out.println(A + B);
-
-        // 스캐너를 닫음
-        scanner.close();
+        
+        // A와 B의 합을 계산
+        int sum = A + B;
+        
+        // 결과 출력
+        System.out.println(sum);
     }
 }
