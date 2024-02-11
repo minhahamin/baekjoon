5660a41d98176a483900be9e5a783e2d2679a932
HongMinha <131649405+minhahamin@users.noreply.github.com>
2024-02-11 12:49:18 +0900

    [Bronze V] Title: A×B, Time: 200 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // 두 정수 A와 B 입력받기
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        
+        // A와 B를 곱한 결과 출력
+        System.out.println(A * B);
+    }
+}
