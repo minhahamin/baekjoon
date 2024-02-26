cf4f5e2940e8d07c844ad9346d22ee1160e87b3b
HongMinha <131649405+minhahamin@users.noreply.github.com>
2024-02-26 19:17:09 +0900

    [Bronze V] Title: 구구단, Time: 244 ms, Memory: 18636 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // 사용자로부터 N을 입력 받음
+        int N = scanner.nextInt();
+        
+        // 구구단 출력
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
+        }
+        
+        scanner.close();
+    }
+}
